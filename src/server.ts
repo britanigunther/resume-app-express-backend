e9c7c21bdbfbab2113f895552a18a8c96b39e737
Britani Gunther <guntherbritani@gmail.com>
2025-11-03 23:08:35 -0600

    Add Mongo connection to server and update tsconfig module to ES2022 to allow top level await

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT!;
+
+await connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
