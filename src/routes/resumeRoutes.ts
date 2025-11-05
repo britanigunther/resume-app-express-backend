bcb177a9b15d58205518f2cf1bf868b89cd69e45
Britani Gunther <guntherbritani@gmail.com>
2025-11-04 20:34:54 -0600

    Create a post endpoint for inserting a new resume into the db

--- src/routes/resumeRoutes.ts
+++ src/routes/resumeRoutes.ts
@@ -4,8 +4,28 @@ import Resume from "../models/Resume.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const resumes = await Resume.find();
-  res.json(resumes);
+  try {
+    const resumes = await Resume.find();
+    return res.status(200).json(resumes);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const resume = new Resume(req.body);
+    const savedResume = await Resume.create(resume);
+    return res.status(201).json(savedResume);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      if (error.name === "ValidationError") {
+        return res.status(400).send(error.message);
+      }
+    }
+    return res.status(500).send("Server error");
+  }
 });
 
 export default router;
